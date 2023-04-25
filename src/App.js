2230343ebf25adac628bd2c57efb99705a6e7d4d
NicoR <nicolas.roemershaeuser@web.de>
2023-04-25 12:27:50 +0200

    updates..

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ function App() {
   return (
     <>
       <div className="flex flex-col justify-center  container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <p className="text-3xl text-gray-700 font-bold mb-2 text-center">
-          Welcome!
-        </p>
-        <p className="text-gray-500 text-lg text-center">
-          React and Tailwind CSS in action
-        </p>
+
 
         <Infosection />
+
+
+        
         <Footer />
 
       </div>
