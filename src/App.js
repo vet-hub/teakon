df6d38a381415c31dd1ea39cba3ca8649aef40f6
NicoR <nicolas.roemershaeuser@web.de>
2023-04-25 11:03:41 +0200

    infosection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
+import Infosection from './components/Infosection';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-      <h1 class="text-9xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">
+          Welcome!
+        </p>
+        <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
+        </p>
+
+        <Infosection />
+ 
+      </div>
+    </>
+
   );
 }
-
 export default App;
