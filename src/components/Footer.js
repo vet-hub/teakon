facbc1fd2fe5d670f563e0cf17dc0a604dfe8272
NicoR <nicolas.roemershaeuser@web.de>
2023-04-25 12:05:45 +0200

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,24 +8,47 @@ export default function Footer() {
                     <p>Teakon-Masala GmbH.<br />Providing reliable infos since 2023</p>
                 </div>
                 <div>
-                    <span className="footer-title">Services</span>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <span className="footer-title">Sorten</span>
+                    <a className="link link-hover">Schwarzer Tee</a>
+                    <a className="link link-hover">Grüner Tee</a>
+                    <a className="link link-hover">Oolong Tee</a>
+                    <a className="link link-hover">Kräutertee</a>
+                    <a className="link link-hover">Früchtetee</a>
+                    <a className="link link-hover">Rooiboistee</a>
                 </div>
                 <div>
-                    <span className="footer-title">Company</span>
-                    <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <span className="footer-title">Anbaugebiet</span>
+                    <a className="link link-hover">Assam us</a>
+                    <a className="link link-hover">China</a>
+                    <a className="link link-hover">Darjeeling</a>
+                    <a className="link link-hover">Japan</a>
+                    <a className="link link-hover">Nepal</a>
+                    <a className="link link-hover">Südkorea</a>
+
                 </div>
                 <div>
-                    <span className="footer-title">Legal</span>
-                    <a className="link link-hover">Terms of use</a>
-                    <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <span className="footer-title">Wirkung</span>
+                    <a className="link link-hover">Anregend</a>
+                    <a className="link link-hover">Erfrischend</a>
+                    <a className="link link-hover">Wärmend</a>
+                </div>
+                <div>
+                    <span className="footer-title">Geschmack</span>
+                    <a className="link link-hover">Cremig</a>
+                    <a className="link link-hover">Duftig-spritzig</a>
+                    <a className="link link-hover">Kräftig</a>
+                    <a className="link link-hover">Säurearme Früchtetees</a>
+                    <a className="link link-hover">Umami</a>
+                    <a className="link link-hover">Würzig</a>
+                    <a className="link link-hover">Zitronig</a>
+                </div>
+                <div>
+                    <span className="footer-title">Zutaten</span>
+                    <a className="link link-hover">Chai</a>
+                    <a className="link link-hover">Earl Grey / Bergamotte</a>
+                    <a className="link link-hover">Ingwer</a>
+                    <a className="link link-hover">Kamille</a>
+                    <a className="link link-hover">Minze</a>
                 </div>
             </footer>
 
