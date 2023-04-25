cce5a7c957245313c46e8299b75444ee1b662835
NicoR <nicolas.roemershaeuser@web.de>
2023-04-25 11:31:36 +0200

    infosec+footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './App.css';
 import Infosection from './components/Infosection';
-
+import Footer from './components/Footer'
 
 
 function App() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-        <p className="text-3xl text-gray-700 font-bold mb-5">
+      <div className="flex flex-col justify-center  container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-2 text-center">
           Welcome!
         </p>
-        <p className="text-gray-500 text-lg">
+        <p className="text-gray-500 text-lg text-center">
           React and Tailwind CSS in action
         </p>
 
         <Infosection />
- 
+        <Footer />
+
       </div>
     </>
 
