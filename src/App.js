dfe48f05a09b1f43933534d05a5eeadf3e37e9fc
vet <vetefir@gmail.com>
2023-04-27 09:39:00 +0200

    temp commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ function App() {
       <h1 class="text-9xl font-bold underline">
         Hello world!
       </h1>
+
+		<div class="collapse">
+  <input type="checkbox" class="peer" />
+  <div class="collapse-title bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+    Click me to show/hide content
+  </div>
+  <div class="collapse-content bg-primary text-primary-content peer-checked:bg-secondary peer-checked:text-secondary-content">
+    <p>hello</p>
+  </div>
+</div>
+
     </div>
   );
 }
