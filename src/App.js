9c2fe0e7adb5ffc7f3a4181fcbea575a8e0ba302
Tyrosius <florian.schoof@gmx.de>
2023-04-27 16:13:39 +0200

    criteria search implemented (not tested)

--- src/App.js
+++ src/App.js
@@ -17,8 +17,49 @@ import Footer from './components/Footer'
 
 function App() {
   const [chosenCriterias,setChosenCriterias] = useState([]);
+  const [allTeas,setAllTeas] = useState([]);/* mit useEffect und fetch/axios füllen */
+  const [resultTeas, setResultTeas] = useState([]);
+
   const searchFunktion=()=>{
-    
+    let foundTeas =[...allTeas];
+    const chosenCriteriasTemp=[...chosenCriterias];
+    const nameSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="name");
+    const teeartSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="teas");
+    const originSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="origin");
+    const flavourSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="coffein");
+    const benefitSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="flavour");
+    const coffeinSearch=chosenCriteriasTemp.filter(criteria=>criteria.category==="effect");
+    if (nameSearch.length>0){
+      /* foundTeas=foundTeas.filter(tea=>tea.name===name) */
+    }else /* wenn kein tee namentlich gesucht wird */
+    {
+    if (teeartSearch.length>0){
+      const foundInCategory=[]
+      teeartSearch.map(criteria=>{
+        const foundByCriteria=foundTeas.filter(tea=>tea.teeart===criteria)
+        foundInCategory.push(...foundByCriteria)
+      })
+      foundTeas=foundInCategory 
+    }
+    if (originSearch.length>0){
+      const foundInCategory=[]
+      originSearch.map(criteria=>{
+        const foundByCriteria=foundTeas.filter(tea=>tea.anbaugebiet===criteria)
+        foundInCategory.push(...foundByCriteria)
+      })
+      foundTeas=foundInCategory
+    }
+    if (flavourSearch.length>0){
+      teeartSearch.map(criteria=>{foundTeas=foundTeas.filter(tea=>tea.aromen===criteria)})/* bei und */
+      
+    }
+    if (benefitSearch.length>0){
+      teeartSearch.map(criteria=>{foundTeas=foundTeas.filter(tea=>tea.benefits===criteria)})
+    }
+    if (coffeinSearch.length>0){
+      foundTeas=foundTeas.filter(tea=>tea.coffein===coffeinSearch[0].name)
+    }}
+    setResultTeas(foundTeas)
   }
   return (
 
