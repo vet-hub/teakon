0e4fe390ab98e1bece438a01c7f5071e927032c3
Tyrosius <florian.schoof@gmx.de>
2023-05-05 16:01:09 +0200

    saubere verbindng auch über bild, etc..

--- src/components/CriteriaSelection.js
+++ src/components/CriteriaSelection.js
@@ -35,6 +35,7 @@ export default function CriteriaSelection ({chosenCriterias, setChosenCriterias,
                     <h3 className="italic criteriaName font-bold text-3xl self-center text-black ">Wirkung</h3>
                 </div>
             </div>
+            {/*TODO checkbox und Selectedcriteria erst versteckt deutlichere interaktivität */}
             <CheckboxContainer selectedCategory={selectedCategory} chosenCriterias={chosenCriterias} setChosenCriterias={setChosenCriterias} allTeearten={allTeearten} allAnbaugebiete={allAnbaugebiete} allBenefits={allBenefits} allAromen={allAromen} allCoffein={allCoffein} />
             <p className="text-black mt-5">Ausgewählte Kriterien:</p>
             <SelectedCriterias chosenCriterias={chosenCriterias} setChosenCriterias={setChosenCriterias} />
