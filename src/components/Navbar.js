6ad55742f422cc75227af3ac4b78c4be8b4c0927
Uyansu <suat1593@web.de>
2023-05-08 17:35:50 +0200

    burger menue and more responsive work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,37 +72,58 @@ export default function Navbar({setChosenCriterias,allTeearten,allAnbaugebiete,a
         setChosenCriterias([{name:e.target.innerHTML, checked: true, category:"teas"}])
                 navigateTo("/ergebnisse/suche")
     }
-
-    return (
-        
+    const isSmallScreen = window.innerWidth < 500;
+    return (   
+        <>
+        {isSmallScreen ? ( <div className="navbar bg-base-100 rounded-box pb-0">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    {allTeearten.map((teeart)=>{return(
+                                    <li key={teeart.name}><div key={teeart.name} onClick={addCategory}>{teeart.name}</div></li>
+                                )
+                    }   )  }
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <Link to="/"><img className="w-36" src={MyLogo}></img></Link>
+            </div>
+            <div className="navbar-end">
+            </div>
+        </div>
+        ) : (
         <div className="navbar bg-gray-300 dark:bg-gray-800 p-3 rounded-box">
             {/* ---------------- Logo section -------------- */}
             <div className="flex-1 px-2 lg:flex-none">
                 <Link to="/"><img src={MyLogo}></img></Link>
             </div> 
-
-        <div className="flex justify-end flex-1 px-2">
-        <div className="flex items-stretch">
-            {/* ----------------- Searchbar ---------------- */}
-            <form className="form-control" onSubmit={resolveSearchBar}>
-                <input type="search" placeholder="Suche" className="input input-bordered" onChange={inputHandler}  />
-            </form>
-            {/* ------------------ zubehör ----------------- */}
-            <Link className="btn btn-ghost rounded-btn" to="/zubehör">Zubehör</Link>
-            {/* ---------------- Dropdown menü ------------- */}
-            <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost rounded-btn">Tee</label>
-                <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
-                    {allTeearten.map((teeart)=>{return(
-                        <li key={teeart.name}><div key={teeart.name} onClick={addCategory}>{teeart.name}</div></li>
-                    )
-                    })}
-                </ul>
-
+            <div className="flex justify-end flex-1 px-2">
+                <div className="flex items-stretch">
+                    {/* ----------------- Searchbar ---------------- */}
+                    <form className="form-control" onSubmit={resolveSearchBar}>
+                        <input type="search" placeholder="Suche" className="input input-bordered" onChange={inputHandler}  />
+                    </form>
+                    {/* ------------------ zubehör ----------------- */}
+                    <Link className="btn btn-ghost rounded-btn" to="/zubehör">Zubehör</Link>
+                    {/* ---------------- Dropdown menü ------------- */}
+                    <div className="dropdown dropdown-end">
+                        <label tabIndex={0} className="btn btn-ghost rounded-btn">Tee</label>
+                        <ul tabIndex={0} className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                            {allTeearten.map((teeart)=>{return(
+                                <li key={teeart.name}><div key={teeart.name} onClick={addCategory}>{teeart.name}</div></li>
+                            )
+                            })}
+                        </ul>
+                    </div>
+                </div>
             </div>
-
-        </div>
-        </div>
-        </div>
+        </div>)
+        }
+        </>    
+        
     )
 }
