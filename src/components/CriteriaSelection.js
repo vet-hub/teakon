1ea1bdf1499a7b688995704fa75416cc4397ce71
NicoR <nicolas.roemershaeuser@web.de>
2023-05-09 18:05:55 +0200

    clean-up

--- src/components/CriteriaSelection.js
+++ src/components/CriteriaSelection.js
@@ -17,7 +17,7 @@ export default function CriteriaSelection ({chosenCriterias, setChosenCriterias,
 /* TODO responsiveness */
 return(
 <div className="criteriaSelection flex flex-col items-center w-full p-5">
-    <h2 className="italic mb-2 lg:mb-10 font-bold md:m-5 max-[350px]:text-2xl  min-[351px]:text-3xl md:text-4xl text-black" >Kriterien Auswahl</h2>
+    <h2 className="italic mb-2 lg:mb-10 font-bold md:m-5 max-[350px]:text-2xl  min-[351px]:text-3xl md:text-4xl text-black dark:text-gray-300" >Kriterien Auswahl</h2>
     <div className="mainCriterias text-center md:flex md:justify-evenly md:flex-wrap w-full md:gap-2 ">
         
         <div className="min-h-8 mt-3 md:criteriaSelector md:opacity-80 shadow border visible border-black rounded-xl md:flex md:justify-center md:w-1/6 md:mb-5 md:aspect-square md:hover:cursor-pointer bg-teas-bg md:bg-cover"  onClick={setToTeas } >
@@ -41,7 +41,7 @@ return(
     </div> 
         </div>
             {selectedCategory && <CheckboxContainer selectedCategory={selectedCategory} chosenCriterias={chosenCriterias} setChosenCriterias={setChosenCriterias} allTeearten={allTeearten} allAnbaugebiete={allAnbaugebiete} allBenefits={allBenefits} allAromen={allAromen} allCoffein={allCoffein} />}
-            {chosenCriterias.length>0 && <><p className="text-black mt-5">Ausgewählte Kriterien:</p>
+            {chosenCriterias.length>0 && <><p className="text-black dark:text-gray-300 mt-5">Ausgewählte Kriterien:</p>
             <SelectedCriterias chosenCriterias={chosenCriterias} setChosenCriterias={setChosenCriterias} /></>}
             <button onClick={startSearch} className="bg-black hover:bg-white text-white font-bold hover:text-black py-3 px-7 mt-7 mb-7 border border-black hover:border-black rounded-full duration-200">Ergebnisse anzeigen</button>
         </div>
